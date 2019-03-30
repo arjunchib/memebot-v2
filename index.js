60396af2041416579a1b67c810ffc6c10d0cb70f
Arjun Chib <arjunpatrickchib@gmail.com>
2019-03-30 12:13:19 -0400

    Add sharding

--- index.js
+++ index.js
@@ -1,37 +1,7 @@
-const fs = require('fs')
-const Discord = require('discord.js')
-const { prefix, token } = require('./config.json')
+const { ShardingManager } = require('discord.js')
+const { token } = require('./config.json')
 
-const client = new Discord.Client()
-client.commands = new Discord.Collection()
+const manager = new ShardingManager('./bot.js', { token: token })
 
-const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'))
-
-for (const file of commandFiles) {
-	const command = require(`./commands/${file}`)
-	client.commands.set(command.name, command)
-}
-
-client.once('ready', () => {
-  console.log('Ready!')
-})
-
-client.on('message', message => {
-	if (!message.content.startsWith(prefix) || message.author.bot) return
-
-	const args = message.content.slice(prefix.length).split(/ +/)
-	const commandName = args.shift().toLowerCase()
-
-	if (!client.commands.has(commandName)) return
-
-  const command = client.commands.get(commandName)
-
-	try {
-    command.execute(message, args)
-	} catch (error) {
-    console.error(error)
-    message.reply('there was an error trying to execute that command!')
-	}
-})
-
-client.login(token)
+manager.spawn()
+manager.on('launch', shard => console.log(`Launched shard ${shard.id}`))
