18d7a108497ce983b6768c7e05bfee3d15165298
Arjun Chib <arjunpatrickchib@gmail.com>
2019-07-22 01:55:34 -0400

    Cleanup unneeded files and env vars

--- src/commands/add.js
+++ src/commands/add.js
@@ -1,56 +1,59 @@
 const validator = require('validator')
 
 module.exports = {
-	name: 'add',
-	description: 'creates a meme from a youtube link',
-	usage: 'add <url> <start> <end> <name> [aliases...]',
-	minArgs: 4,
-	execute(message, args) {
+  name: 'add',
+  description: 'creates a meme from a youtube link',
+  usage: 'add <url> <start> <end> <name> [aliases...]',
+  minArgs: 4,
+  execute(message, args) {
+    const data = {}
 
-		const data = {}
+    // Parse url
+    const hostWhitelist = ['www.youtube.com', 'youtube.com', 'youtu.be']
 
-		// Parse url
-		const hostWhitelist = [
-			'www.youtube.com',
-			'youtube.com',
-			'youtu.be'
-		]
+    if (!validator.isURL(args[0], { host_whitelist: hostWhitelist })) {
+      return message.channel.send('the URL must be a youtube link')
+    }
 
-		if (!validator.isURL(args[0], { host_whitelist: hostWhitelist })) {
-			return message.channel.send('the URL must be a youtube link')
-		}
+    data.url = args[0]
 
-		data.url = args[0]
+    // Parse start and end
+    const longPattern = /^-?(\d{1,2}:)?[0-5]?\d:[0-5]\d(.\d+)?$/
+    const shortPattern = /^-?\d+(.\d+)?$/
 
-		// Parse start and end
-		const longPattern = /^-?(\d{1,2}:)?[0-5]?\d:[0-5]\d(.\d+)?$/
-		const shortPattern = /^-?\d+(.\d+)?$/
+    const invalidTimes = []
 
-		const invalidTimes = []
+    if (
+      !validator.matches(args[1], longPattern) &&
+      !validator.matches(args[1], shortPattern)
+    ) {
+      invalidTimes.push('start')
+    }
 
-		if (!validator.matches(args[1], longPattern) &&
-				!validator.matches(args[1], shortPattern)) {
-			invalidTimes.push('start')
-		}
+    if (
+      !validator.matches(args[2], longPattern) &&
+      !validator.matches(args[2], shortPattern)
+    ) {
+      invalidTimes.push('end')
+    }
 
-		if (!validator.matches(args[2], longPattern) &&
-				!validator.matches(args[2], shortPattern)) {
-			invalidTimes.push('end')
-		}
+    if (invalidTimes.length > 0) {
+      return message.channel.send(
+        `the ${invalidTimes.join(
+          ' and '
+        )} time must match \`[-][HH:]MM:SS[.m...]\` or \`[-]S+[.m...]\``
+      )
+    }
 
-		if (invalidTimes.length > 0) {
-			return message.channel.send(`the ${ invalidTimes.join(' and ') } time must match \`[-][HH:]MM:SS[.m...]\` or \`[-]S+[.m...]\``)
-		}
+    data.start = args[1]
+    data.end = args[2]
 
-		data.start = args[1]
-		data.end = args[2]
+    // Parse name and aliases
+    data.name = args[3]
+    data.aliases = []
 
-		// Parse name and aliases
-		data.name = args[3]
-		data.aliases = []
-
-		for (let i = 4; i < args.length; i++) {
-			data.aliases.push(args[i])
-		}
-	}
+    for (let i = 4; i < args.length; i++) {
+      data.aliases.push(args[i])
+    }
+  }
 }
--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
+const path = require('path')
 const { ShardingManager } = require('discord.js')
-const production = (process.env.NODE_ENV == 'production')
+require('dotenv').config()
 
-const { token } = production
-  ? require('./.config-prod.json')
-  : require('./.config-dev.json')
-
-const manager = new ShardingManager('./bot.js', { token: token })
+const botPath = path.resolve(__dirname, 'bot.js')
+const manager = new ShardingManager(botPath, {
+  token: process.env.DISCORD_TOKEN
+})
 
 manager.spawn()
 manager.on('launch', shard => console.log(`Launched shard ${shard.id}`))
