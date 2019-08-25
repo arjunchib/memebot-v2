23e9019bef3620ed033fc84516cce62535d1801b
Arjun Chib <arjunpatrickchib@gmail.com>
2019-08-25 09:45:58 -0400

    Move parsing logic to functions

--- src/commands/add.js
+++ src/commands/add.js
@@ -4,12 +4,70 @@ const CommandError = require('../CommandError.js')
 
 const hostWhitelist = ['www.youtube.com', 'youtube.com', 'youtu.be']
 
+function getURL(args) {
+  if (!validator.isURL(args[0], { host_whitelist: hostWhitelist })) {
+    throw new CommandError('the URL must be a youtube link')
+  }
+  return args[0]
+}
+
+function getTimes(args) {
+  const longPattern = /^-?(\d{1,2}:)?[0-5]?\d:[0-5]\d(.\d+)?$/
+  const shortPattern = /^-?\d+(.\d+)?$/
+
+  const invalidTimes = []
+
+  if (
+    !validator.matches(args[1], longPattern) &&
+    !validator.matches(args[1], shortPattern)
+  ) {
+    invalidTimes.push('start')
+  }
+
+  if (
+    !validator.matches(args[2], longPattern) &&
+    !validator.matches(args[2], shortPattern)
+  ) {
+    invalidTimes.push('end')
+  }
+
+  if (invalidTimes.length > 0) {
+    throw new CommandError(
+      `the ${invalidTimes.join(
+        ' and '
+      )} time must match \`[-][HH:]MM:SS[.m...]\` or \`[-]S+[.m...]\``
+    )
+  }
+
+  return {
+    start: args[1],
+    end: args[2]
+  }
+}
+
+function getAliases(args) {
+  const aliases = []
+
+  for (let i = 4; i < args.length; i++) {
+    aliases.push(args[i])
+  }
+
+  return aliases
+}
+
 module.exports = {
   name: 'add',
   description: 'creates a meme from a youtube link',
   usage: 'add <url> <start> <end> <name> [aliases...]',
   minArgs: 4,
   execute(message, args) {
+    const data = {}
+    data.url = getURL(args)
+    Object.assign(data, getTimes(args))
+    data.name = args[3]
+    data.aliases = getAliases(args)
+
+    /*
     const query = `{
       memes {
         name
@@ -22,53 +80,6 @@ module.exports = {
     }`
 
     client.request(query).then(data => console.log(data))
-
-    const data = {}
-
-    // Parse url
-    if (!validator.isURL(args[0], { host_whitelist: hostWhitelist })) {
-      throw new CommandError('the URL must be a youtube link')
-    }
-
-    data.url = args[0]
-
-    // Parse start and end
-    const longPattern = /^-?(\d{1,2}:)?[0-5]?\d:[0-5]\d(.\d+)?$/
-    const shortPattern = /^-?\d+(.\d+)?$/
-
-    const invalidTimes = []
-
-    if (
-      !validator.matches(args[1], longPattern) &&
-      !validator.matches(args[1], shortPattern)
-    ) {
-      invalidTimes.push('start')
-    }
-
-    if (
-      !validator.matches(args[2], longPattern) &&
-      !validator.matches(args[2], shortPattern)
-    ) {
-      invalidTimes.push('end')
-    }
-
-    if (invalidTimes.length > 0) {
-      throw new CommandError(
-        `the ${invalidTimes.join(
-          ' and '
-        )} time must match \`[-][HH:]MM:SS[.m...]\` or \`[-]S+[.m...]\``
-      )
-    }
-
-    data.start = args[1]
-    data.end = args[2]
-
-    // Parse name and aliases
-    data.name = args[3]
-    data.aliases = []
-
-    for (let i = 4; i < args.length; i++) {
-      data.aliases.push(args[i])
-    }
+    */
   }
 }
