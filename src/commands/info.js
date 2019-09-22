293551e541024eadeb7fdb46a30dffced20699c4
Arjun Chib <arjunpatrickchib@gmail.com>
2019-09-22 11:13:24 -0400

    Add info command

---
+++ src/commands/info.js
@@ -0,0 +1,40 @@
+const client = require('../client.js')
+const CommandError = require('../CommandError.js')
+
+const codeBlockify = str => `\`\`\`${str}\`\`\``
+
+module.exports = {
+  name: 'info',
+  description: 'get info about a meme',
+  usage: 'info <command>',
+  minArgs: 1,
+  async execute(message, args) {
+    const command = args[0]
+
+    const query = `query Info($command: String!){
+      meme(command: $command) {
+        _id
+        name
+        author {
+          id
+          name
+        }
+        url
+        commands
+        tags
+        volume
+        createdAt
+      }
+    }`
+
+    try {
+      const info = await client.request(query, { command })
+      message.channel.send(codeBlockify(JSON.stringify(info.meme, null, 2)))
+    } catch (error) {
+      console.error(error)
+      throw new CommandError(
+        'Something went wrong when getting info on this meme'
+      )
+    }
+  }
+}
