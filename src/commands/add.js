2ec7533f24e2ac3bb326741db1c6eeebd1f06fd3
Matthew Hannah <matthew.r.hannah@gmail.com>
2020-01-31 18:19:33 -0500

    Update add to only provide youtube URL

--- src/commands/add.js
+++ src/commands/add.js
@@ -1,50 +1,11 @@
-const validator = require('validator')
 const client = require('../client.js')
 const CommandError = require('../CommandError.js')
-const ytdl = require('ytdl-core')
-const ffmpeg = require('fluent-ffmpeg')
-const path = require('path')
 
 function getURL(args) {
-  const hostWhitelist = ['www.youtube.com', 'youtube.com', 'youtu.be']
-
-  if (
-    !validator.isURL(args[0], { host_whitelist: hostWhitelist }) &&
-    ytdl.validateURL(args[0])
-  ) {
-    throw new CommandError('The URL must be a youtube link')
-  }
   return args[0]
 }
 
 function getTimes(args) {
-  const longPattern = /^-?(\d{1,2}:)?[0-5]?\d:[0-5]\d(.\d+)?$/
-  const shortPattern = /^-?\d+(.\d+)?$/
-
-  const invalidTimes = []
-
-  if (
-    !validator.matches(args[1], longPattern) &&
-    !validator.matches(args[1], shortPattern)
-  ) {
-    invalidTimes.push('start')
-  }
-
-  if (
-    !validator.matches(args[2], longPattern) &&
-    !validator.matches(args[2], shortPattern)
-  ) {
-    invalidTimes.push('end')
-  }
-
-  if (invalidTimes.length > 0) {
-    throw new CommandError(
-      `the ${invalidTimes.join(
-        ' and '
-      )} time must match \`[-][HH:]MM:SS[.m...]\` or \`[-]S+[.m...]\``
-    )
-  }
-
   return {
     start: args[1],
     end: args[2]
@@ -59,23 +20,6 @@ function getAliases(args) {
   }
 
   return aliases
-}
-
-function downloadFile(data) {
-  const stream = ytdl(data.url, { filter: 'audio' })
-  const outputFile = path.resolve('.cache', 'audio', `${data.name}.mp3`)
-  ffmpeg(stream)
-    .noVideo()
-    .seekOutput(data.start)
-    .format('mp3')
-    .outputOptions(['-write_xing 0', `-to ${data.end}`])
-    .on('end', () => {
-      console.log('Saved file!')
-    })
-    .on('error', () => {
-      throw new CommandError('Something went wrong when downloading your meme')
-    })
-    .save(outputFile)
 }
 
 module.exports = {
@@ -94,10 +38,8 @@ module.exports = {
       name: message.member.displayName
     }
 
-    const outputFile = path.resolve('.cache', 'audio', `${meme.name}.mp3`)
-
-    const query = `mutation CreateMeme($name: String!, $authorID: ID!, $authorName: String!, $url: String!){
-      createMeme(name: $name, author: {id: $authorID, name: $authorName}, url: $url) {
+    const query = `mutation CreateMeme($name: String!, $authorID: ID!, $authorName: String!, $url: String!, $start: String!, $end,: String!){
+      createMeme(name: $name, author: {id: $authorID, name: $authorName}, url: $url, start: $start, end: $end) {
         _id
         name
         author {
@@ -117,9 +59,10 @@ module.exports = {
         name: meme.name,
         authorID: meme.author.id,
         authorName: meme.author.name,
-        url: outputFile
+        url: meme.url,
+        start: meme.start,
+        end: meme.end
       })
-      downloadFile(meme)
     } catch (error) {
       console.error(error)
       console.error(JSON.stringify(error, undefined, 2))
