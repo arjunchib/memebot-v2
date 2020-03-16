a423a36f8783b0c9cc0e132e87152d5302c13ac4
Arjun Chib <arjunpatrickchib@gmail.com>
2020-03-16 13:49:51 -0400

    Add workflow for deployment

---
+++ scripts/pm2-config.js
@@ -0,0 +1,10 @@
+const fs = require('fs')
+
+const envVariables = process.argv.slice(2)
+const pm2ConfigPath = `${process.env.GITHUB_WORKSPACE}/ecosystem.config.json`
+
+const pm2Config = JSON.parse(fs.readFileSync(pm2ConfigPath))
+
+envVariables.forEach(x => (pm2Config.apps[0].env[x] = process.env[x]))
+
+fs.writeFileSync(pm2ConfigPath, JSON.stringify(pm2Config))
--- src/client.js
+++ src/client.js
@@ -1,5 +1,5 @@
 const { GraphQLClient } = require('graphql-request')
 
-const endpoint = process.env.MEMEBOT_API_ENDPOINT + '/graphql'
+const endpoint = process.env.MEMEBOT_API_BASE_URL + '/graphql'
 
 module.exports = new GraphQLClient(endpoint, { headers: {} })
