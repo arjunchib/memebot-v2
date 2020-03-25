e614b3259bf40a7bc3c88ca77702d90832e6e130
Arjun Chib <arjunpatrickchib@gmail.com>
2020-03-25 19:34:08 -0400

    Refacror unalias command

--- src/commands/unalias/index.js
+++ src/commands/unalias/index.js
@@ -1,5 +1,4 @@
-const graphqlClient = require('../../graphql-client')
-const CommandError = require('../../utils/CommandError')
+const execute = require('./execute')
 
 module.exports = {
   name: 'unalias',
@@ -7,32 +6,5 @@ module.exports = {
   usage: 'unalias <name> <alias>',
   minArgs: 2,
   maxArgs: 2,
-  async execute(message, args) {
-    const name = args[0]
-    const alias = args[1]
-
-    const query = `mutation RemoveAliasFromMeme($name: String!, $alias: String!){
-      removeAliasFromMeme(name: $name, alias: $alias) {
-        _id
-        name
-        author {
-          id
-          name
-        }
-        url
-        commands
-        tags
-        volume
-        createdAt
-      }
-    }`
-
-    try {
-      await graphqlClient.request(query, { name, alias })
-      message.channel.send(`Removed command ${alias} from ${name}`)
-    } catch (error) {
-      console.error(error)
-      throw new CommandError('Something went wrong when untagging this meme')
-    }
-  }
+  execute
 }
