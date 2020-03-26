a0ab9e201f75cb784ff08fad91ae12c06911d224
Arjun Chib <arjunpatrickchib@gmail.com>
2020-03-26 01:23:49 -0400

    Refacror volume command

--- src/commands/volume/index.js
+++ src/commands/volume/index.js
@@ -1,5 +1,4 @@
-const graphqlClient = require('../../graphql-client')
-const CommandError = require('../../utils/CommandError')
+const execute = require('./execute')
 
 module.exports = {
   name: 'volume',
@@ -7,40 +6,5 @@ module.exports = {
   usage: 'volume <name> <volume>',
   minArgs: 2,
   maxArgs: 2,
-  async execute(message, args) {
-    const name = args[0]
-    const volume = Number(args[1])
-
-    if (!Number.isFinite(volume) || volume <= 0) {
-      throw new CommandError(
-        'Volume must be a non-zero, positive, finite number'
-      )
-    }
-
-    const query = `mutation UpdateVolumeOfMeme($name: String!, $volume: Float!){
-      updateVolumeOfMeme(name: $name, volume: $volume) {
-        _id
-        name
-        author {
-          id
-          name
-        }
-        url
-        commands
-        tags
-        volume
-        createdAt
-      }
-    }`
-
-    try {
-      await graphqlClient.request(query, { name, volume })
-      message.channel.send(`Set volume of ${name} to ${volume}`)
-    } catch (error) {
-      console.error(error)
-      throw new CommandError(
-        'Something went wrong when setting the volume of this meme'
-      )
-    }
-  }
+  execute
 }
